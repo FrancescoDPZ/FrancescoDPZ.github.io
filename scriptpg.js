a66536fbe893b0958f0c26d82dfc7cc126456612
FrancescoDPZ <38539670+FrancescoDPZ@users.noreply.github.com>
2018-05-30 14:39:51 -0400

    v3.3
    img - figcaption

--- scriptpg.js
+++ scriptpg.js
@@ -3,20 +3,20 @@
 function myFunction() {
 
     // Declare variables
-    var input, filter, photo, figcaption, img, i;
+    var input, filter, photo, img, figcaption, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     photo = document.getElementById("photo");
-    figcaption = photo.getElementsByTagName("figcaption");
+    img = photo.getElementsByTagName("img");
 
 
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < figcaption.length; i++) {
-        img = figcaption[i].getElementsByTagName("img")[0];
-        if (img.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            figcaption[i].style.display = "";
+    for (i = 0; i < img.length; i++) {
+        figcaption = img[i].getElementsByTagName("figcaption")[0];
+        if (figcaption.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            img[i].style.display = "";
         } else {
-            figcaption[i].style.display = "none";
+            img[i].style.display = "none";
         }
     }
 }
