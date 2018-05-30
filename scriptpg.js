6cfe4825417f1707b115af9d52b7751b3eaef387
FrancescoDPZ <38539670+FrancescoDPZ@users.noreply.github.com>
2018-05-30 14:22:27 -0400

    v3.1
    search JS

--- scriptpg.js
+++ scriptpg.js
@@ -3,7 +3,7 @@
 function myFunction() {
 
     // Declare variables
-    var input, filter, photo, figcaption, a, i;
+    var input, filter, photo, figcaption, img, i;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     photo = document.getElementById("photo");
@@ -12,8 +12,8 @@ function myFunction() {
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < figcaption.length; i++) {
-        a = figcaption[i].getElementsByTagName("a")[0];
-        if (figcaption.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        img = figcaption[i].getElementsByTagName("img")[0];
+        if (img.innerHTML.toUpperCase().indexOf(filter) > -1) {
             figcaption[i].style.display = "";
         } else {
             figcaption[i].style.display = "none";
